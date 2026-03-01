a4ce0d5fabe253d287eb0cb80a7ed54a1ee7382d
aakthepaak <aakarsh3110@gmail.com>
2026-02-28 19:50:17 -0500

    Implemented Graph structure and node functionalities

---
+++ src/nodes/blog_node.py
@@ -0,0 +1,32 @@
+from src.states.blogstate import BlogState
+
+class BlogNode:
+    """
+    A class to represent blog nodes
+    """
+
+    def __init__(self,llm):
+        self.llm = llm
+    
+    def title_creation(self,state:BlogState):
+        """
+        Create title for the blog
+        """
+
+        if "topic" in state and state["topic"]:
+            prompt="""
+                You are an expert blog content writer. Use Markdown formatting. Generate a blog title for the {topic}. This title should be creative and SEO friendly.
+                """
+            
+            system_message=prompt.format(topic=state["topic"])
+            response = self.llm.invoke(system_message)
+            return {"blog":{"title":response.content}}
+        
+    def content_generation(self,state:BlogState):
+        if "topic" in state and state["topic"]:
+            prompt="""
+                You are an expert blog writer. Use Markdown formatting to create detailed blog content with expanded breakdown for this {topic}.
+                """
+            system_message = prompt.format(topic=state["topic"])
+            response = self.llm.invoke(system_message)
+            return {"blog":{"title":state['blog','title'],"content":response.content}}
